0c6cb305521d5d5433698f93dbb60ea31489f9f3
LXirgil <hikkyhachiman0920@gmail.com>
2024-07-18 10:55:24 +0900

    期末課題　再提出

---
+++ Maze/src/Maze/start.java
@@ -0,0 +1,15 @@
+package Maze;
+public class start extends MyFrame {
+	
+	static boolean spacePressed;
+ 
+	public static void main(String[] args) {
+		
+		MyFrame maze =new Maze2(15,15);
+		//maze.show();
+	}
+	
+ 
+ 
+}
+ 
